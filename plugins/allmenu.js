c3aab1a1a69397baba3e8115850a0ce8a088279a
Progress Tech <progresstech237@gmail.com>
2026-09-14 20:40:26 +0100

    Update allmenu.js

--- plugins/allmenu.js
+++ plugins/allmenu.js
@@ -96,8 +96,7 @@ function buildMenu(pushName) {
         `${config.CHANNEL_LINK}`,
     ]);
 
-    return `
-╔═══════════════════════╗
+    return '
   *👑 ✦ 𝐌𝐎𝐍𝐀 𝐋𝐈𝐒𝐀 𝐕𝟐 🤭 ✦ 👑*
 ${'  '}${pick(TAGLINES)}
 ╚═══════════════════════╝
